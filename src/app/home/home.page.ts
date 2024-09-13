8bebf99118ef0c517ceeba85a47598ebed189a4c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-13 13:05:39 -0300

    ac

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor( public router : Router) {}
+
+  email: string = '';
+  nome: string = '';
+  message: string = '';
+  secretKey: string = 'pipipopo';
+  encryptedMessage: string = '';
+  encryptedEmail: string = '';
+  encryptedName: string = '';
+
+
+  sendData(){
+    if (this.message && this.secretKey) {
+      const encryptedM = CryptoJS.AES.encrypt(this.message, this.secretKey).toString();
+      this.encryptedMessage = encryptedM;
+      const encryptedE = CryptoJS.AES.encrypt(this.email, this.secretKey).toString();
+      this.encryptedEmail = encryptedE;
+      const encryptedN = CryptoJS.AES.encrypt(this.nome, this.secretKey).toString();
+      this.encryptedName = encryptedN;
+    }
+    if(this.encryptedMessage){
+      const encodedMessage = encodeURIComponent(this.encryptedMessage);
+      const encodedEmail = encodeURIComponent(this.encryptedEmail);
+      const encodedName = encodeURIComponent(this.encryptedName);
+      this.router.navigateByUrl
+      (`/tela2/${encodedMessage}/${encodedEmail}/${encodedName}`);
+    }
+  }
+}
